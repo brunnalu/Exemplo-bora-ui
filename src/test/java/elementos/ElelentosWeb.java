c16c8201343b68941769f30565ad1284b876df93
repoe2e <brrunalu@gmail.com>
2024-08-24 13:54:15 -0300

    inclusão do projeto

---
+++ src/test/java/elementos/ElelentosWeb.java
@@ -0,0 +1,14 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class ElelentosWeb {
+	
+	By btnLogin =By.xpath("'//button[@class='login-button']'");
+	By username =By.xpath("//input[@name='username']");
+	By password =By.xpath("//input[@name='password']");
+	By btnContinuar =By.xpath("//button[@class='form-button'];");
+	By btnCancelar = By.xpath("//button[@class='form-button secondary-button']");
+	
+	
+}
